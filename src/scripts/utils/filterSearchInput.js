72bbc922136bcd898a4912da0d379fb95ba51ab0
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-07-01 16:57:20 +0200

    Added delete icon to filters's search bar

--- src/scripts/utils/filterSearchInput.js
+++ src/scripts/utils/filterSearchInput.js
@@ -1,5 +1,7 @@
 const filtersInput = document.querySelectorAll(".filter-search-input");
+const deleteIcon = document.querySelectorAll(".delete-icon");
 
+// filter the search if we write in input
 export function filterListItems(listElement, searchValue) {
     if (!listElement) {
         return;
@@ -13,13 +15,28 @@ export function filterListItems(listElement, searchValue) {
     })
 }
 
-filtersInput.forEach(input => {
+// For each filter's input
+filtersInput.forEach((input, index) => {
+    const icon = deleteIcon[index];
+    icon.style.display = "none";
+
+    // Everytime we type in the input ...
     input.addEventListener("input", (event) => {
         const searchValue = event.target.value.toLowerCase();
         const type = event.target.dataset.type; // "ingredients", "appliance", "ustensils"
         const list = document.getElementById(`${type}List`);
 
+        // Display delete icon if searchbar's length > 0
+        icon.style.display = searchValue.length > 0 ? "" : "none";
         console.log("Filtrage:", type, list);
+
         filterListItems(list, searchValue);
-    })
+    });
+
+    // Hide the delete icon on click and restart the filter
+    icon.addEventListener("click", () => {
+        input.value = "";
+        input.dispatchEvent(new Event("input")); // restart filter
+    });
+
 })
