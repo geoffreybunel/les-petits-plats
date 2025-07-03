f1094cd446947f1891dc787d19ea01ea2cc74615
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-07-03 13:44:31 +0200

    changed tags.js location

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,7 +1,7 @@
 import { recipes } from '../../data/recipes.js';
 import { dropdown, displayLists } from '../components/filterDropdown.js';
 import { displayRecipes } from '../components/recipeCard.js';
-import { displayTags } from '../utils/tags.js';
+import { displayTags } from '../components/tags.js';
 import { filterListItems } from '../utils/filterSearchInput.js';
 
 export default function Home() {
